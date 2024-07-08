8480f4718667661c5e11dbbd059e65e98681861c
sangjunchung <wjtkwn19@gmail.com>
2024-07-08 11:54:42 +0900

    gameTwoStep

--- my-simple-website/src/component/GameTwoStep.js
+++ my-simple-website/src/component/GameTwoStep.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const GameTwoStep = () => {
   const [guess, setGuess] = useState();
   const [message, setMessage] = useState('정답을 맞춰주세요!');
   const [number, setNumber] = useState(Math.floor(Math.random() * 30) + 1);
   const [tryto, setTryto] = useState(0);
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const checkGuess = (e) => {
     setGuess(e.target.value);
@@ -18,6 +20,7 @@ const GameTwoStep = () => {
 
     if(userGuess === number){
         setMessage('축하합니다! 정답입니다!');
+        setIsCorrect(true);
     } else if (userGuess > number){
         setMessage('숫자가 너무 큽니다!');
     } else {
@@ -26,6 +29,14 @@ const GameTwoStep = () => {
 
     setGuess('');
   };
+
+  const restart = () => {
+    setGuess('');
+    setMessage('정답을 맞춰주세요!')
+    setNumber(Math.floor(Math.random()*30)+1);
+    setTryto(0);
+    setIsCorrect(false);
+  }
 
   return (
     <div>
@@ -42,6 +53,8 @@ const GameTwoStep = () => {
       <br />
       <p>횟수 : {tryto}</p>
       <p> 결과 : {message}</p>
+
+      {isCorrect ? (<Link to="/game"><button>처음으로</button></Link>) : (<button onClick={restart}>재시작버튼</button>)}
     </div>
   );
 };
