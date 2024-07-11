eefb15f70965a33ba4f9d0a902eb480802101272
sangjunchung <wjtkwn19@gmail.com>
2024-07-11 10:18:46 +0900

    useEffect timer

--- my-simple-website/src/component/TicTapToe.js
+++ my-simple-website/src/component/TicTapToe.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './TicTapToe.css';
 import { Link } from "react-router-dom";
-/*
-useEffect(()=>{},[])
-*/
+
 const shuffleArray = (Array) => {
   // sort = 정렬
   // Math.random() - 0.5 : 배열을 랜덤으로 섞을 때 자주 사용 (-0.5 ~ 0.5 사이 생성)
@@ -26,6 +24,20 @@ const TicTapToe = () => {
   const [message, setMessage] = useState('숫자를 순서대로 눌러주세요!');
   
   const [isCorrect, setIsCorrect] = useState(false);
+  
+  const [timer, setTimer] = useState(5);
+
+  useEffect(()=>{
+    let countdown;
+
+    if(timer > 0){
+      countdown = setTimeout(()=>{
+       setTimer(timer - 1)
+      }, 1000);
+    } else if (timer === 0){
+      alert("시간초과! 게임이 종료되었습니다.");
+    }
+  });
 
   const clickNumber = (number) => {
     // 만약 사용자가 클릭해야되는 숫자와 사용자가 클릭한 숫자가 서로 일치하는가?
@@ -46,11 +58,13 @@ const TicTapToe = () => {
     setNextNumber(1);
     setMessage('숫자를 순서대로 눌러주세요!');
     setIsCorrect(false);
+    setTimer(5);
   };
 
   return (
     <div className="tic-tap-toe-container">
-      <h1>틱탭토</h1>
+      <h1>틱탭토 1단계</h1>
+      <div className="timer">남은 시간 : {timer} 초</div>
       <div className="tictaptoe-grid">
         {numbers.map((number) => (
           <button className="tictaptoe-button" key={number} onClick={() => clickNumber(number)}>
--- my-simple-website/src/component/TicTapToeTwoStep.js
+++ my-simple-website/src/component/TicTapToeTwoStep.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './TicTapToe.css';
 import { Link } from "react-router-dom";
 
@@ -6,6 +6,7 @@ const shuffleArray = (Array) => {
     return Array.sort(() => Math.random() - 0.5);
   };
 
+/* TicTapToeTwoStep 컴포넌트 시작 위치 */
 const TTT_twoStep = () => {
     const [numbers, setNumbers] = useState(
         shuffleArray([...Array(20).keys()].map((n) => n + 1))
@@ -14,7 +15,23 @@ const TTT_twoStep = () => {
       const [nextNumber, setNextNumber] = useState(1);
       const [message, setMessage] = useState('숫자를 순서대로 눌러주세요!');
       const [isCorrect, setIsCorrect] = useState(false);
-    
+      const [timer, setTimer] = useState(10); // 초기 시간 설정
+
+      //useEffect(function 기능명() => {어떤 기능이 동작해야하나요?},[어떤값이변경될때마다 function기능이 움직여야하나요?]);
+      //useEffect(() => {},[]);
+      //useEffect(() => {},[numbers]); // numbers 숫자가 변경될 때마다 function 기능 발생
+
+      useEffect(()=>{
+        let countdown;
+        if (timer > 0){
+          countdown = setTimeout(()=>{
+            setTimer(timer - 1);
+          }, 1000);
+        } else if (timer === 0){
+          alert("시간초과! 게임이 종료되었습니다.");
+        }
+      });
+
       const clickNumber = (number) => {
         if (number === nextNumber) {
           if (number === 20) {
@@ -33,11 +50,13 @@ const TTT_twoStep = () => {
         setNextNumber(1);
         setMessage('숫자를 순서대로 눌러주세요!');
         setIsCorrect(false);
+        setTimer(10);
       };
 
     return(
       <div className="tic-tap-toe-container">
-      <h1>틱탭토</h1>
+      <h1>틱탭토 2단계</h1>
+      <div className="timer">남은 시간 : {timer} 초</div>
       <div className="tictaptoe2-grid">
         {numbers.map((number) => (
           <button className="tictaptoe-button" key={number} onClick={() => clickNumber(number)}>
