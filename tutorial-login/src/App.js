038b7ed53d0257a60f520a5e20fd799ec5206bf4
sangjunchung <wjtkwn19@gmail.com>
2024-07-17 14:34:18 +0900

    login

--- tutorial-login/src/App.js
+++ tutorial-login/src/App.js
@@ -3,6 +3,7 @@ import LoginContext from './components/LoginContext';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import './App.css';
+import TodoList from './components/TodoList';
 
 /* 현재 App.js는 제일 위에 있는 컴포넌트 = 객체 */
 function App() {
@@ -28,6 +29,10 @@ function App() {
       {/* 로그인을 해야 TodoList 확인 가능 */}
 
       <Login />
+
+      <hr />
+      {/* 로그인이 되었을 때 로그인한 회원의 TodoList 출력 */}
+      {loginMember && ( <TodoList />)}
 
       {/* value={} -> 하나의 값만 작성
           value={{}} -> 두가지 이상의 값을 작성 
